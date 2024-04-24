413ad4ac1b8377585cda7f93f0b73876644b5dc4
Damiano <115373289+Damiana214155255@users.noreply.github.com>
2024-04-24 23:26:36 +0200

    Dodaj pliki projektów.

---
+++ Literalnie/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Literalnie
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Zasady popup = new Zasady();
+            popup.ShowDialog();
+        }
+    }
+
+}
